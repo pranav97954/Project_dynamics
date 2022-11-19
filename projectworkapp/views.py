6818db85d51461aa29bd64543ea8c29cdef527bf
betarohit777 <rohit.pandey@fintricity.com>
2022-11-19 12:39:02 +0530

    changes

--- projectworkapp/views.py
+++ projectworkapp/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+    return render(request,"index.html")
+
+def login(request):
+    return render(request,"login.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def documentation(request):
+    return render(request,"documentation.html")
+
+def simulation(request):
+    return render(request,"simulation.html")
+
+def slide(request):
+    return render(request,"slide.html")
+
+def visualization(request):
+    return render(request,"visualization.html")
