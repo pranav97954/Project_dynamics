945f275d161dd3d752e52423a1fd0f04d1357030
betarohit777 <rohit.pandey@fintricity.com>
2022-12-21 23:33:40 +0530

    upload with new folder

--- projectworkapp/forms.py
+++ projectworkapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms  
-class StudentForm(forms.Form):  
+class SubmitForm(forms.Form):  
     firstname = forms.CharField(label="Enter first name",max_length=50)  
     lastname  = forms.CharField(label="Enter last name", max_length = 10)  
-    email     = forms.EmailField(label="Enter Email")  
+    email     = forms.EmailField(label="Enter Email") 
+    foldername =  forms.CharField(label="Enter folder name",max_length=50)  
     file      = forms.FileField() # for creating file input
--- projectworkapp/function.py
+++ projectworkapp/function.py
@@ -1,4 +1,6 @@
-def handle_uploaded_file(f):  
-    with open('projectworkapp/upload/'+f.name, 'wb+') as destination:  
+import os
+def handle_uploaded_file(f,foldername):  
+    os.makedirs('projectworkapp/upload/'+foldername)
+    with open('projectworkapp/upload/'+foldername+'/'+f.name, 'wb+') as destination:  
         for chunk in f.chunks():  
             destination.write(chunk) 
